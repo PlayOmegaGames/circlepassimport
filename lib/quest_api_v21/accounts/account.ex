c2c4a20015f67ee05153cef603408bc1ee1dcec0
Ubuntu <jay@cryptoconcierge.consulting>
2023-09-14 17:05:50 -0700

    fixed many to many between collection points and accounts

--- lib/quest_api_v21/accounts/account.ex
+++ lib/quest_api_v21/accounts/account.ex
@@ -1,6 +1,7 @@
 defmodule QuestApiV21.Accounts.Account do
   use Ecto.Schema
   import Ecto.Changeset
+  import Ecto.Query  # <-- Add this line if you plan to use Ecto queries in this module
 
   @primary_key {:id, :binary_id, autogenerate: true}
   @foreign_key_type :binary_id
@@ -8,17 +9,23 @@ defmodule QuestApiV21.Accounts.Account do
     field :email, :string
     field :hashed_passowrd, :string
     field :name, :string
-    many_to_many :collection_points, QuestApiV21.Collection_Points.Collection_Point, join_through: "collectionpoints_accounts"
+    many_to_many :collection_points, QuestApiV21.Collection_Points.Collection_Point, join_through: "collectionpoints_accounts", join_keys: [account_id: :id, collectionpoint_id: :id]
 
     timestamps()
   end
 
   @doc false
   def changeset(account, attrs) do
+    collection_points = prepare_collection_points(attrs)  # Fetch and validate collection_points based on IDs
+
     account
     |> cast(attrs, [:name, :email, :hashed_passowrd])
     |> validate_required([:name, :email, :hashed_passowrd])
-    |> cast_assoc(:collection_points, with: &QuestApiV21.Collection_Points.Collection_Point.changeset/2)
+    |> put_assoc(:collection_points, collection_points)  # Use put_assoc for existing collection_points
+  end
 
+  defp prepare_collection_points(attrs) do
+    collection_point_ids = Map.get(attrs, "collection_points", [])
+    QuestApiV21.Repo.all(from cp in QuestApiV21.Collection_Points.Collection_Point, where: cp.id in ^collection_point_ids)
   end
 end
--- lib/quest_api_v21/collection__points/collection__point.ex
+++ lib/quest_api_v21/collection__points/collection__point.ex
@@ -1,6 +1,7 @@
 defmodule QuestApiV21.Collection_Points.Collection_Point do
   use Ecto.Schema
   import Ecto.Changeset
+  import Ecto.Query
 
   @primary_key {:id, :binary_id, autogenerate: true}
   @foreign_key_type :binary_id
@@ -13,17 +14,23 @@ defmodule QuestApiV21.Collection_Points.Collection_Point do
     belongs_to :business, QuestApiV21.Businesses.Business
     belongs_to :quest, QuestApiV21.Quests.Quest
     belongs_to :collector, QuestApiV21.Collectors.Collector
-    many_to_many :accounts, QuestApiV21.Accounts.Account, join_through: "collectionpoints_accounts"
-
+    many_to_many :accounts, QuestApiV21.Accounts.Account, join_through: "collectionpoints_accounts", join_keys: [collectionpoint_id: :id, account_id: :id]
 
     timestamps()
   end
 
   @doc false
   def changeset(collection__point, attrs) do
+    accounts = prepare_accounts(attrs)  # Fetch and validate accounts based on IDs
+
     collection__point
     |> cast(attrs, [:name, :image, :scans, :redirect_url, :badge_description, :business_id, :quest_id, :collector_id])
     |> validate_required([:name, :image, :redirect_url])
-    |> cast_assoc(:accounts, with: &QuestApiV21.Accounts.Account.changeset/2)
+    |> put_assoc(:accounts, accounts)  # Use put_assoc for existing accounts
+  end
+
+  defp prepare_accounts(attrs) do
+    account_ids = Map.get(attrs, "accounts", [])
+    QuestApiV21.Repo.all(from a in QuestApiV21.Accounts.Account, where: a.id in ^account_ids)
   end
 end
