2fb533447d7fbc2a4a21f305580cd0cf4209e1bd
theJayBecker <124844075+theJayBecker@users.noreply.github.com>
2023-11-18 22:13:18 -0800

    Update migrate.sh

--- scripts/migrate.sh
+++ scripts/migrate.sh
@@ -2,7 +2,7 @@
 # This script runs database migrations
 
 # Define the container name
-CONTAINER_NAME=quest-api-container
+CONTAINER_NAME=692454124440.dkr.ecr.us-east-1.amazonaws.com/quest-api
 
 # Run the Ecto migrations within the container
 sudo docker exec $CONTAINER_NAME mix ecto.migrate
