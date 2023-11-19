218d35b88cd19bafecc15a04b2f10e2d1b41dcbb
theJayBecker <124844075+theJayBecker@users.noreply.github.com>
2023-11-18 21:44:02 -0800

    Create migrate.sh

---
+++ scripts/migrate.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# This script runs database migrations
+
+# Define the container name
+CONTAINER_NAME=quest-api-container
+
+# Run the Ecto migrations within the container
+sudo docker exec $CONTAINER_NAME mix ecto.migrate
