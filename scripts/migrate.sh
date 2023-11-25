c157924ba8d22e7fdd3a1d6196e9a446524dd293
Jay Becker <jay@cryptoconcierge.consulting>
2023-11-24 17:35:00 -0800

    Docker stuff

--- scripts/migrate.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# This script runs database migrations
-
-# Define the container name
-CONTAINER_NAME=quest-api
-
-# Run the Ecto migrations within the container
-sudo docker exec $CONTAINER_NAME mix ecto.migrate
