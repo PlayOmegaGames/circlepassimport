9a7c3b277cabcdb20736146824713ff218062c0f
Jay Becker <jay@cryptoconcierge.consulting>
2023-11-27 16:57:01 +0000

    got confetti working locally

--- lib/quest_api_v21_web.ex
+++ lib/quest_api_v21_web.ex
@@ -17,7 +17,7 @@ defmodule QuestApiV21Web do
   those modules here.
   """
 
-  def static_paths, do: ~w(assets fonts images favicon.ico robots.txt)
+  def static_paths, do: ~w(assets fonts images js favicon.ico robots.txt)
 
   def router do
     quote do
