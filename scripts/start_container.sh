eb5ca8cba3e3e3cb00bcbd285485a1bed07b82f2
Jay Becker <jay@cryptoconcierge.consulting>
2024-01-03 21:40:08 -0800

    updated appspec

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -4,4 +4,4 @@ cd /home/ubuntu/
 
 
 # Run the new container
-docker run --env-file .env --name quest-api-v2 -p 4000:4000 692454124440.dkr.ecr.us-east-1.amazonaws.com/quest-api
+docker run -d --env-file .env --name quest-api-v2 -p 4000:4000 692454124440.dkr.ecr.us-east-1.amazonaws.com/quest-api
