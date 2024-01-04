6fbd7b60cf2a046d67fa13804b44e3f6b677d3bb
Jay Becker <jay@cryptoconcierge.consulting>
2024-01-03 21:32:40 -0800

    updated appspec

--- scripts/pull_container.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-ECR_REPOSITORY=692454124440.dkr.ecr.us-east-1.amazonaws.com/quest-api
-IMAGE_TAG=latest
-
-# Authenticate Docker to the ECR Repository
-sudo aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin $ECR_REPOSITORY
-
-# Pull the latest image
-docker pull $ECR_REPOSITORY:$IMAGE_TAG
