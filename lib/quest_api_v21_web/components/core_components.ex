026db9cf5aa55df00e8775e537dfa860aada4dd0
Jay Becker <jay@cryptoconcierge.consulting>
2024-01-12 22:10:12 +0000

    refactored components

--- lib/quest_api_v21_web/components/core_components.ex
+++ lib/quest_api_v21_web/components/core_components.ex
@@ -39,35 +39,7 @@ defmodule QuestApiV21Web.CoreComponents do
   end
 
 
-  @doc """
-  Renders the bottom navbar icons text
 
-  ## Examples
-
-  <.bottom_nav page_name="home" icon="home" />
-  """
-
-
-
-  attr :url, :string, required: true
-  attr :request_path, :string, required: true
-  attr :page_name, :string, required: true
-  attr :icon, :string, required: true
-
-  def bottom_nav(assigns) do
-    ~H"""
-    <div class={calculate_class(assigns)}>
-      <a href={@url} class="">
-        <.icon name={"hero-#{@icon}"} class="ml-1 w-7 h-7" />
-        <p class="text-sm text-center"><%= @page_name %></p>
-      </a>
-    </div>
-    """
-  end
-
-  defp calculate_class(assigns) do
-    if assigns.request_path == assigns.url, do: "text-violet-700", else: "text-slate-700"
-  end
 
 
   @doc """
@@ -637,6 +609,33 @@ defmodule QuestApiV21Web.CoreComponents do
     ~H"""
     <span class={[@name, @class]} />
     """
+  end
+
+    @doc """
+  Renders the bottom navbar icons text
+
+  ## Examples
+
+  <.bottom_nav page_name="home" icon="home" />
+  """
+  attr :url, :string, required: true
+  attr :request_path, :string, required: true
+  attr :page_name, :string, required: true
+  attr :icon, :string, required: true
+
+  def bottom_nav(assigns) do
+    ~H"""
+    <a href={@url} class="py-2">
+        <div class={calculate_class(assigns)}>
+          <.icon name={"hero-#{@icon}"} class="ml-1 w-7 h-7" />
+          <p class="text-sm text-center"><%= @page_name %></p>
+      </div>
+      </a>
+    """
+  end
+
+  defp calculate_class(assigns) do
+    if assigns.request_path == assigns.url, do: "text-violet-700", else: "text-slate-700"
   end
 
   ## JS Commands
