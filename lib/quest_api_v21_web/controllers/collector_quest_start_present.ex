311eaefa71203b1f9902eb691580faafc17b567a
Jay Becker <jay@cryptoconcierge.consulting>
2024-01-20 04:46:43 +0000

    got  template on logic for redirect badges

--- lib/quest_api_v21_web/controllers/collector_quest_start_present.ex
+++ lib/quest_api_v21_web/controllers/collector_quest_start_present.ex
@@ -7,25 +7,35 @@ defmodule QuestApiV21Web.CollectorQuestStartPresent do
           Plug.Conn.t(),
           atom() | %{:badges => any(), :quest_start => any(), optional(any()) => any()}
         ) :: Plug.Conn.t()
-  def handle_present_quest_start(conn, collector) do
-    IO.inspect(collector, label: "handle_present_quest_start data")
-    account = Accounts.get_account!(conn.assigns.current_user.id)
-    quest = Quests.get_quest(collector.quest_start)
-    badge = find_associated_badge(collector, quest)
+    def handle_present_quest_start(conn, collector) do
+      IO.inspect(collector, label: "handle_present_quest_start data")
+      account = Accounts.get_account!(conn.assigns.current_user.id)
+      quest = Quests.get_quest(collector.quest_start)
+      badge = find_associated_badge(collector, quest)
 
-    if badge do
-      if Enum.any?(account.badges, fn b -> b.id == badge.id end) do
-        Logger.info("User already has badge ID: #{badge.id}. No action taken.")
+      if badge do
+        if Enum.any?(account.badges, fn b -> b.id == badge.id end) do
+          Logger.info("User already has badge ID: #{badge.id}. No action taken.")
+        else
+          add_badge_and_create_scan(account, badge)
+          add_quest_to_user(account, quest)
+        end
+
+        if badge.unauthorized_url do
+          # Render redirect template if unauthorized_url is present
+          conn
+          |> assign(:body_class, "bg-gradient-to-b from-purple-400 to-brand h-screen bg-no-repeat")
+          |> render("redirect.html", %{badge: badge})
+        else
+          # Render normal collector template
+          render(conn, "collector.html", collector: collector, badge: badge)
+        end
       else
-        add_badge_and_create_scan(account, badge)
-        add_quest_to_user(account, quest)
+        Logger.error("No badge found associated with both Collector ID: #{collector.id} and Quest ID: #{quest.id}")
+        # Handle the error case, possibly by rendering a different template or redirecting
       end
-    else
-      Logger.error("No badge found associated with both Collector ID: #{collector.id} and Quest ID: #{quest.id}")
     end
 
-    render(conn, "collector.html", collector: collector, badge: badge)
-  end
 
   defp add_badge_and_create_scan(account, badge) do
     case Accounts.add_badge_to_user(account.id, badge) do
