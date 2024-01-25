dbc584e90fadf51f806a127a88b889ccaa0ad667
Jay Becker <jay@cryptoconcierge.consulting>
2024-01-25 20:03:44 +0000

    added frosted glass and solid icons

--- lib/quest_api_v21_web/components/layouts.ex
+++ lib/quest_api_v21_web/components/layouts.ex
@@ -2,8 +2,11 @@ defmodule QuestApiV21Web.Layouts do
   use QuestApiV21Web, :html
 
   def calculate_class(request_path, url) do
-    if request_path == url, do: "text-violet-700", else: "text-slate-700"
+    if request_path == url, do: "font-medium", else: "font-light	"
   end
 
+  def calculate_icon(request_path, url) do
+    if request_path == url, do: "-solid", else: ""
+  end
   embed_templates "layouts/*"
 end
