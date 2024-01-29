b912751bfbf12472da69bb09653911f0e97308c2
Jay Becker <jay@cryptoconcierge.consulting>
2024-01-29 18:46:41 +0000

    Tweaked styles for badges page

--- lib/quest_api_v21_web/components/core_components.ex
+++ lib/quest_api_v21_web/components/core_components.ex
@@ -66,7 +66,7 @@ defmodule QuestApiV21Web.CoreComponents do
     ~H"""
     <div class={assigns[:class]} data-redirect-url={assigns[:redirect_url]} data-badge-description={assigns[:badge_description]} data-is-user-badge={assigns[:is_user_badge]}>
       <!-- Badge image and name -->
-        <img src={assigns[:image]} alt={assigns[:name]} class="rounded-full ring-2">
+        <img src={assigns[:image]} alt={assigns[:name]} class="rounded-full badge-neo">
         <p class="text-center mt-2 text-xs text-slate-700"><%= assigns[:name] %></p>
     </div>
     """
@@ -180,15 +180,12 @@ defmodule QuestApiV21Web.CoreComponents do
 
   def reward(assigns) do
     ~H"""
-    <div class={"p-1 inline-flex text-#{@color}-600 bg-#{@color}-100 rounded-lg"}>
-
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-            class="ml-2 my-auto w-4 h-full">
-        <path stroke-linecap="round" stroke-linejoin="round" d="M16.5 18.75h-9m9 0a3 3 0 0 1 3 3h-15a3 3 0 0 1 3-3m9 0v-3.375c0-.621-.503-1.125-1.125-1.125h-.871M7.5 18.75v-3.375c0-.621.504-1.125 1.125-1.125h.872m5.007 0H9.497m5.007 0a7.454 7.454 0 0 1-.982-3.172M9.497 14.25a7.454 7.454 0 0 0 .981-3.172M5.25 4.236c-.982.143-1.954.317-2.916.52A6.003 6.003 0 0 0 7.73 9.728M5.25 4.236V4.5c0 2.108.966 3.99 2.48 5.228M5.25 4.236V2.721C7.456 2.41 9.71 2.25 12 2.25c2.291 0 4.545.16 6.75.47v1.516M7.73 9.728a6.726 6.726 0 0 0 2.748 1.35m8.272-6.842V4.5c0 2.108-.966 3.99-2.48 5.228m2.48-5.492a46.32 46.32 0 0 1 2.916.52 6.003 6.003 0 0 1-5.395 4.972m0 0a6.726 6.726 0 0 1-2.749 1.35m0 0a6.772 6.772 0 0 1-3.044 0" />
-        </svg>
-
-        <h1 class="text-[13px] my-auto mx-2"><%= @text %></h1>
-    </div>
+      <div class={"inline-flex text-slate-600 bg-slate-100 rounded-full"}>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+              class="ml-2 my-auto w-4 h-full">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M16.5 18.75h-9m9 0a3 3 0 0 1 3 3h-15a3 3 0 0 1 3-3m9 0v-3.375c0-.621-.503-1.125-1.125-1.125h-.871M7.5 18.75v-3.375c0-.621.504-1.125 1.125-1.125h.872m5.007 0H9.497m5.007 0a7.454 7.454 0 0 1-.982-3.172M9.497 14.25a7.454 7.454 0 0 0 .981-3.172M5.25 4.236c-.982.143-1.954.317-2.916.52A6.003 6.003 0 0 0 7.73 9.728M5.25 4.236V4.5c0 2.108.966 3.99 2.48 5.228M5.25 4.236V2.721C7.456 2.41 9.71 2.25 12 2.25c2.291 0 4.545.16 6.75.47v1.516M7.73 9.728a6.726 6.726 0 0 0 2.748 1.35m8.272-6.842V4.5c0 2.108-.966 3.99-2.48 5.228m2.48-5.492a46.32 46.32 0 0 1 2.916.52 6.003 6.003 0 0 1-5.395 4.972m0 0a6.726 6.726 0 0 1-2.749 1.35m0 0a6.772 6.772 0 0 1-3.044 0" />
+          </svg>
+      </div>
     """
   end
 
