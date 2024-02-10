6441fb42af1c32a8331b891dd4bc4cffc1007a1c
Jay Becker <jay@cryptoconcierge.consulting>
2024-02-10 23:10:43 +0000

    now it grabs public urls instead of presigned ones from s3

--- lib/quest_api_v21_web/controllers/qr_generator..ex
+++ lib/quest_api_v21_web/controllers/qr_generator..ex
@@ -1,50 +1,29 @@
 defmodule QuestApiV21Web.QrGenerator do
   # Function to create a QR code from the given url and upload it to AWS S3
   def create_and_upload_qr(url) do
-    # Attempt to create and render a QR code from the provided url
     case request_qrcode(url) do
-      # If QR code creation and rendering succeed
       {:ok, qr_code} ->
-        # Define the AWS S3 bucket name where the QR code will be stored
         bucket = "quest-api-resources"
-
-        # Construct the S3 path for the QR code, using a unique identifier based on the url
         s3_path = "qr-codes/#{unique_identifier(url)}.png"
 
-        # Upload the generated QR code to S3 and handle the response
-        # Note: This line seems redundant and may be a mistake, as the operation is repeated below.
-        ExAws.S3.put_object(bucket, s3_path, qr_code)
-        |> ExAws.request()
-        |> handle_s3_response()
-
-        # Attempt to upload the QR code to S3 again and handle the outcome
         case ExAws.S3.put_object(bucket, s3_path, qr_code) |> ExAws.request() do
-          # If upload succeeds, generate and return a presigned URL for accessing the uploaded QR code
           {:ok, _response} ->
-            generate_presigned_url(bucket, s3_path) # Return just the URL string
+            # Construct the public URL
+            # Make sure your S3 bucket is configured for public access if you use this method
+            public_url = "https://#{bucket}.s3.amazonaws.com/#{s3_path}"
+            {:ok, public_url}
 
-          # If upload fails, return the error
-          {:error, error} -> {:error, error}
+          {:error, error} ->
+            {:error, error}
         end
 
-      # If QR code creation and rendering fail, return the error
-      {:error, reason} -> {:error, reason}
+      {:error, reason} ->
+        {:error, reason}
     end
   end
 
   # Generates a unique identifier for the QR code based on the provided url
   defp unique_identifier(url), do: "#{url}-#{:erlang.unique_integer([:positive])}"
-
-  # Generates a presigned URL for the uploaded QR code, allowing secure access without making the object public
-  defp generate_presigned_url(bucket, object_path) do
-    ExAws.S3.presigned_url(ExAws.Config.new(:s3), :get, bucket, object_path, query_params: [{"response-content-disposition", "attachment"}])
-  end
-
-  # Helper function to process successful S3 responses
-  defp handle_s3_response({:ok, _response}), do: :ok
-
-  # Helper function to process error responses from S3
-  defp handle_s3_response({:error, error}), do: {:error, error}
 
   def request_qrcode(url) do
     api_url = "https://qrcode-monkey.p.rapidapi.com/qr/custom"
