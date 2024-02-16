2b827169afb7ed848f32c255b2ea206337d77405
Jay Becker <jay@cryptoconcierge.consulting>
2024-02-16 02:10:58 +0000

    enabled dashboard for production

--- lib/quest_api_v21_web/router.ex
+++ lib/quest_api_v21_web/router.ex
@@ -144,7 +144,7 @@ defmodule QuestApiV21Web.Router do
   # end
 
   # Enable LiveDashboard and Swoosh mailbox preview in development
-  if Application.compile_env(:quest_api_v21, :dev_routes) do
+  #if Application.compile_env(:quest_api_v21, :dev_routes) do
     # If you want to use the LiveDashboard in production, you should put
     # it behind authentication and allow only admins to access it.
     # If your application does not have an admins-only section yet,
@@ -158,7 +158,7 @@ defmodule QuestApiV21Web.Router do
       live_dashboard "/dashboard", metrics: QuestApiV21Web.Telemetry
       forward "/mailbox", Plug.Swoosh.MailboxPreview
     end
-  end
+  #end
 
   ## Authentication routes
 
