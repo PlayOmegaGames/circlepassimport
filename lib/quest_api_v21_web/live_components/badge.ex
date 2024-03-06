d316ba4f4a3ea65900f9cf89870db8e6a57ebb6c
Michael Bianchi <michaelbianchi1231@gmail.com>
2024-03-06 20:52:01 +0000

    Created Badge Component for display on SinglePage LiveView

---
+++ lib/quest_api_v21_web/live_components/badge.ex
@@ -0,0 +1,12 @@
+defmodule QuestApiV21Web.LiveComponents.Badge do
+  use Phoenix.LiveComponent
+
+  def render(assigns) do
+    ~H"""
+    <div>
+      hello world
+    </div>
+    """
+  end
+
+end
