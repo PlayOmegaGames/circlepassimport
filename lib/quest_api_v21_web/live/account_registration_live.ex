04efc27890ef079cec550d2b34f7e28b02a0bd88
Michael Bianchi <michaelbianchi1231@gmail.com>
2024-03-16 00:16:22 +0000

    Created new layout for registration pages with lavender gradient. Made forms transparent.

--- lib/quest_api_v21_web/live/account_registration_live.ex
+++ lib/quest_api_v21_web/live/account_registration_live.ex
@@ -6,7 +6,7 @@ defmodule QuestApiV21Web.AccountRegistrationLive do
 
   def render(assigns) do
     ~H"""
-    <div class="mx-auto max-w-sm">
+    <div class="mx-auto max-w-sm bg-transparent">
       <.header class="text-center">
         Register for an account
         <:subtitle>
