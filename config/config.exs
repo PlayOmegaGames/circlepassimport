abb2ddee44bd1217e00bdfb75fb13882770b6f36
Jay Becker <jay@cryptoconcierge.consulting>
2024-04-02 07:17:03 +0000

    test atyff

--- config/config.exs
+++ config/config.exs
@@ -104,11 +104,5 @@ if Mix.env() != :prod do
   config :git_hooks,
     verbose: true,
     hooks: [
-      pre_commit: [
-        tasks: [
-          {:cmd, "mix format"}
-          # {:cmd, "mix credo"}
-        ]
-      ]
     ]
 end
