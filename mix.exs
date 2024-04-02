2648fec224d3245bc538c03bbac7f296420572eb
Jay Becker <jay@cryptoconcierge.consulting>
2024-04-02 17:37:02 +0000

    test commit

--- mix.exs
+++ mix.exs
@@ -38,7 +38,7 @@ defmodule QuestApiV21.MixProject do
       {:ecto_sql, "~> 3.10"},
       {:postgrex, ">= 0.0.0"},
       {:phoenix_html, "~> 3.3"},
-      {:git_hooks, "~> 0.7.3"},
+      {:git_hooks, "~> 0.7.0", only: [:dev], runtime: false},
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:phoenix_live_view, "~> 0.20.13"},
