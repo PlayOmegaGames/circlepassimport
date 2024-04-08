c60f307d3c875ceadbfb35cf6ff3d908079332d2
theJayBecker <124844075+theJayBecker@users.noreply.github.com>
2024-04-08 12:38:36 -0700

    Update pull_new_image.sh

--- scripts/pull_new_image.sh
+++ scripts/pull_new_image.sh
@@ -2,12 +2,16 @@
 
 cd /home/ubuntu/
 
+# Assuming imagedefinitions.json is in the current directory and contains the imageUri
+
+# Extract the imageUri using jq - make sure jq is installed
+IMAGE_URI=$(jq -r '.[0].imageUri' imagedefinitions.json)
+
 # Log in to ECR
-$(aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 692454124440.dkr.ecr.us-east-1.amazonaws.com)
+$(aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin ${IMAGE_URI%:*})
 
-# Pull the latest Docker image
-docker pull 692454124440.dkr.ecr.us-east-1.amazonaws.com/quest-api:latest
+# Pull the Docker image specified in imagedefinitions.json
+docker pull $IMAGE_URI
 
-#Running migration
-docker run --env-file .env --rm 692454124440.dkr.ecr.us-east-1.amazonaws.com/quest-api /app/bin/quest_api_v21 eval "QuestApiV21.Release.migrate"
-
+# Running migration
+docker run --env-file .env --rm $IMAGE_URI /app/bin/quest_api_v21 eval "QuestApiV21.Release.migrate"
