fa12c5e2579dcaf2ba36abb4827d973c211e041c
theJayBecker <124844075+theJayBecker@users.noreply.github.com>
2024-04-08 16:52:17 -0700

    Update pull_new_image.sh

--- scripts/pull_new_image.sh
+++ scripts/pull_new_image.sh
@@ -1,12 +1,40 @@
 #!/bin/bash
 
+# Assuming this script is executed with root privileges or with a user that has appropriate permissions
+
 cd /home/ubuntu/
 
+# Source the environment variables from the .env file
+if [ -f ".env" ]; then
+    source .env
+else
+    echo "The .env file does not exist. Exiting..."
+    exit 1
+fi
+
+# Construct the image URI using REPOSITORY_URI and ENVIRONMENT variables
+IMAGE_URI="${REPOSITORY_URI}/quest-api:${ENVIRONMENT}"
+
 # Log in to ECR
-$(aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 692454124440.dkr.ecr.us-east-1.amazonaws.com)
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin ${REPOSITORY_URI%/*}
 
-# Pull the latest Docker image
-docker pull 692454124440.dkr.ecr.us-east-1.amazonaws.com/quest-api:latest
+if [ $? -ne 0 ]; then
+    echo "Docker login failed. Exiting..."
+    exit 1
+fi
 
-#Running migration
-docker run --env-file .env --rm 692454124440.dkr.ecr.us-east-1.amazonaws.com/quest-api /app/bin/quest_api_v21 eval "QuestApiV21.Release.migrate"
+# Pull the Docker image using the constructed IMAGE_URI
+docker pull $IMAGE_URI
+
+if [ $? -ne 0 ]; then
+    echo "Failed to pull the Docker image. Exiting..."
+    exit 1
+fi
+
+# Running migration
+docker run --env-file .env --rm $IMAGE_URI /app/bin/quest_api_v21 eval "QuestApiV21.Release.migrate"
+
+if [ $? -ne 0 ]; then
+    echo "Migration command failed. Exiting..."
+    exit 1
+fi
