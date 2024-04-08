704d29990799f57972dff6d36c3367b80a27473c
theJayBecker <124844075+theJayBecker@users.noreply.github.com>
2024-04-08 16:48:34 -0700

    Update pull_new_image.sh

--- scripts/pull_new_image.sh
+++ scripts/pull_new_image.sh
@@ -2,17 +2,11 @@
 
 cd /home/ubuntu/
 
-# Source the environment variables from the .env file
-source .env
-
-# Construct the image URI using REPOSITORY_URI and ENVIRONMENT variables
-IMAGE_URI="$REPOSITORY_URI/quest-api:$ENVIRONMENT"
-
 # Log in to ECR
-$(aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin ${REPOSITORY_URI})
+$(aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 692454124440.dkr.ecr.us-east-1.amazonaws.com)
 
-# Pull the Docker image using the constructed IMAGE_URI
-docker pull $IMAGE_URI
+# Pull the latest Docker image
+docker pull 692454124440.dkr.ecr.us-east-1.amazonaws.com/quest-api:latest
 
-# Running migration
-docker run --env-file .env --rm $IMAGE_URI /app/bin/quest_api_v21 eval "QuestApiV21.Release.migrate"
+#Running migration
+docker run --env-file .env --rm 692454124440.dkr.ecr.us-east-1.amazonaws.com/quest-api /app/bin/quest_api_v21 eval "QuestApiV21.Release.migrate"
