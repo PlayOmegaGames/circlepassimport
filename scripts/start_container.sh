2e29181d8c85f4e882bd570ec8c26fcd117b0778
Jay Becker <jay@cryptoconcierge.consulting>
2024-04-18 13:37:50 +0000

    fixing start container

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,6 +2,15 @@
 
 cd /home/ubuntu/
 
+# Source the environment variables from the .env file
+if [ -f ".env" ]; then
+    source .env
+else
+    echo "The .env file does not exist. Exiting..."
+    exit 1
+fi
+
+IMAGE_URI="${REPOSITORY_URI}:${ENVIRONMENT}"
 
 # Run the new container
 docker run -d --env-file .env --name quest-api-v2 -p 4000:4000 $IMAGE_URI
