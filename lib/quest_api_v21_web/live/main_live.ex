67f0d6f1b0c2975d7b8bd9fd1ee5213aa4de2ac2
Jay Becker <jay@cryptoconcierge.consulting>
2024-04-22 20:15:15 +0000

    formatted

--- lib/quest_api_v21_web/live/main_live.ex
+++ lib/quest_api_v21_web/live/main_live.ex
@@ -320,7 +320,6 @@ defmodule QuestApiV21Web.MainLive do
         <CoreComponents.stats_bubble number={@account.rewards_stats} color="amber" text="Rewards" />
       </div>
     </div>
-
     <!--<div class="w-72 mx-auto rounded-full shadow-md">
       <h1 class="text-center my-4">Share this QR code to your profile</h1>
       <%= raw(@qr_code_svg) %>
